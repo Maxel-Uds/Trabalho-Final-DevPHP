b43d4ca123a64d98ab64f95e31d3fafe5f666cb7
Maxel-Uds <maxellopes32@gmail.com>
2022-03-17 17:30:11 -0300

    preparando projeto para prod

--- database/config.php
+++ database/config.php
@@ -1,6 +1,6 @@
 <?php  
-    define('HOST', 'localhost');
-    define('USERNAME', 'root');		
-    define('PASSWORD', '');			
+    define('HOST', 'us-cdbr-east-05.cleardb.net');
+    define('USERNAME', 'b36a373dcfe740');		
+    define('PASSWORD', '9d004584');			
     define('DBNAME', 'games_db');	
 ?>
