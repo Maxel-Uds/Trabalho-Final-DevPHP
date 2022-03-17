ede3bd5e09b455d26c10103394358b888bbf0c40
Maxel-Uds <maxellopes32@gmail.com>
2022-03-17 17:36:34 -0300

    tentando arrumar o db

--- database/conn.php
+++ database/conn.php
@@ -1,8 +1,6 @@
 <?php  
-    include_once 'config.php';
-
     function conectar()
     {
-        return mysqli_connect(HOST, USERNAME, PASSWORD, DBNAME);
+        return mysqli_connect('us-cdbr-east-05.cleardb.net', 'b36a373dcfe740', '9d004584', 'games_db');
     }
 ?>
